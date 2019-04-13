3efa097188e572ee8ba1241de09fccc1320e6f0e
Marko Radovic <markoradovic996@gmail.com>
2019-04-13 19:09:52 +0200

    Https between siem center and windows agent

--- SIEM/WindowsAgent/src/main/java/com/bsep/WindowsAgent/WindowsAgentApplication.java
+++ SIEM/WindowsAgent/src/main/java/com/bsep/WindowsAgent/WindowsAgentApplication.java
@@ -2,12 +2,20 @@ package com.bsep.WindowsAgent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class WindowsAgentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WindowsAgentApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void doSomethingAfterStartup() {
+		String certificatesTrustStorePath = "C:\\Program Files\\Java\\jre1.8.0_201\\lib\\security\\cacerts";
+		System.setProperty("javax.net.ssl.trustStore", certificatesTrustStorePath);
 	}
 
 }
