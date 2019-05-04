8602152ab1a19323173f0f5d63cc6e8ea6218dc4
Marko Radovic <markoradovic996@gmail.com>
2019-05-04 11:18:46 +0200

    Add method for reading system logs

--- SIEM/WindowsAgent/src/main/java/com/bsep_sbz/WindowsAgent/WindowsAgentApplication.java
+++ SIEM/WindowsAgent/src/main/java/com/bsep_sbz/WindowsAgent/WindowsAgentApplication.java
@@ -1,7 +1,12 @@
 package com.bsep_sbz.WindowsAgent;
 
+import com.bsep_sbz.WindowsAgent.helper.Constants;
+import com.sun.jna.platform.win32.Advapi32Util;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.sun.jna.platform.win32.Advapi32Util.EventLogIterator;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class WindowsAgentApplication {
@@ -10,4 +15,20 @@ public class WindowsAgentApplication {
 		SpringApplication.run(WindowsAgentApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void startupCallback() {
+		new Thread(WindowsAgentApplication::watchSysLogs).start();
+	}
+
+	private static void watchSysLogs() {
+		// Note: if you want to read security logs you must run your ide in administrator mode
+		EventLogIterator iter = new EventLogIterator(Constants.Application);
+		while(iter.hasNext()) {
+			Advapi32Util.EventLogRecord record = iter.next();
+			System.out.println(record.getRecordNumber()
+					+ ": Event ID: " + record.getEventId()
+					+ ", Event Type: " + record.getType()
+					+ ", Event Source: " + record.getSource());
+		}
+	}
 }
---
+++ SIEM/WindowsAgent/src/main/java/com/bsep_sbz/WindowsAgent/helper/Constants.java
@@ -0,0 +1,8 @@
+package com.bsep_sbz.WindowsAgent.helper;
+
+public class Constants
+{
+    public static final String Application = "Application";
+    public static final String Security = "Security";
+    public static final String System = "System";
+}
